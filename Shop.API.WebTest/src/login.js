ae76883385e71c526044136847bb412166751e27
Enot <natasha.cat982404@gmail.com>
2020-04-16 13:55:07 +0300

    Changed fetch

--- Shop.API.WebTest/src/login.js
+++ Shop.API.WebTest/src/login.js
@@ -1,9 +1,8 @@
-
 document.addEventListener('DOMContentLoaded', function() {
     let loginBtn = document.querySelector('#loginBtn');
     let url = 'http://localhost:5000/api/auth/authenticate';
 
-    loginBtn.addEventListener('click', function() {
+    loginBtn.addEventListener('click', async function() {
         let login = document.querySelector('#loginForm').value;
         let password = document.querySelector('#passwordForm').value;
 
@@ -11,54 +10,39 @@ document.addEventListener('DOMContentLoaded', function() {
             'login': login,
             'password': password
         };
-
        
-        let errorBlock = document.querySelector('#errorBlock');    
-        fetch(url, { 
-            method: 'POST',
-            body: JSON.stringify(credentials),
-            headers: {
-                'Content-Type':'application/json'
-            }
-        }).then(response => {
-           
-            
-            if (!response.ok) {
-                    
-                errorBlock.classList.remove('d-none');
-                response.json().then(txt => {
-                    console.log(txt);
-                    errorBlock.innerText = txt.message;
-                });
-                ;
-            } else {
-               
+        let headers = {
+            'Content-Type':'application/json'
+        }
+       
+        let errorBlock = document.querySelector('#errorBlock');   
+        try
+        {
+            let response = await fetch(url, { 
+                                         method: 'POST',
+                                         body: JSON.stringify(credentials),
+                                         headers: headers
+                                        });
+                                        
+            if(response.ok){
+                let responseJson = await response.json();
                 errorBlock.classList.add('d-none');
-                response
-                    .json()
-                        .then(x => {
-                            
-
-                            window.localStorage.setItem('shopapicredentials',x.data.token);
-                            errorBlock.classList.remove('d-none');
-                            errorBlock.classList.remove('alert-danger');
-                            errorBlock.classList.add('alert-success');
-                            errorBlock.innerHTML = `Welcome, ${x.data.name} ${x.data.lastname}`;
-                        
-
-                            // setTimeout(function() {
-                            //     window.location.href = 'categories.html';
-                            // }, 3000);
-                        });
-                        
-                    
+                window.localStorage.setItem('shopapicredentials',responseJson.data.token);
+                errorBlock.classList.remove('d-none');
+                errorBlock.classList.remove('alert-danger');
+                errorBlock.classList.add('alert-success');
+                errorBlock.innerHTML = `Welcome, ${responseJson.data.name} ${responseJson.data.lastname}`;
+            }else{
+                errorBlock.classList.remove('d-none');
+                let responseJson = await response.json();
+                errorBlock.innerText = responseJson.message;
             }
-        })
-        .catch(error => {
+        } 
+        catch(error)
+        {
             errorBlock.classList.remove('d-none');
             errorBlock.classList.add('alert-danger');
             errorBlock.innerHTML = `Unknown error. Failed to connect to service`;
-        });;  
+        }  
     });
-
 });
